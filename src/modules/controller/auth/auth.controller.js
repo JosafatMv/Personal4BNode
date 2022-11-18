b2f8725da6bd61e5bf625d878536270dc0477e23
Josamv <novairesh@gmail.com>
2022-11-18 07:16:42 -0600

    Token response fixed

--- src/modules/controller/auth/auth.controller.js
+++ src/modules/controller/auth/auth.controller.js
@@ -6,7 +6,9 @@ const signin = async (req, res = Response) => {
 	try {
 		const { email, password } = req.body;
 		const token = await login({ email, password });
-		res.status(200).json(token);
+		res.status(200).json({
+			token,
+		});
 	} catch (error) {
 		console.log(error);
 		const message = validateError(error);
